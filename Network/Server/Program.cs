146bc0fb0045c86ef5ef234998d28b7700774b66
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-12-03 15:34:41 +0700

    Server task 3 ready

--- Network/Server/Program.cs
+++ Network/Server/Program.cs
@@ -1,30 +1,15 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
+
 namespace Server;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Message msg = new Message() { Text="Hi", MessageTime = DateTime.Now, RecievedFrom ="Sender", RecievedTo="Reciever" };
-        string serialisedMsg = msg.SerializeMessageToJson();
-        Console.WriteLine(serialisedMsg);
-        Message deserialisedMsg = Message.DeserializeMessageToJson(serialisedMsg);
+        Server server = new Server();
+        server.ServerMsg("Helowossasd");
     }
-    //public bool SendMessage(string message)
-    //{
-    //    using (Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
-    //    {
-    //        var remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
-    //        listener.Blocking = true;
-    //        listener.Bind(remoteEndpoint);
-    //        listener.Listen(100);
-
-    //        Console.WriteLine("wait");
-    //        var socket = listener.Accept();
-
-    //        Console.WriteLine("Connected");
-    //        listener.Close();
-    //    }
-    //}
+    
 }
---
+++ Network/Server/Server.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server;
+
+public class Server
+{
+    public void ServerMsg(string name)
+    {
+        UdpClient udpClient = new UdpClient(12345);
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        Console.WriteLine("Waiting for message...");
+
+        while (true)
+        {
+            byte[] buffer = udpClient.Receive(ref iPEndPoint);
+            if (buffer == null) break;
+            var messageText = Encoding.UTF8.GetString(buffer);
+
+            Message newMessage = Message.DeserializeMessageToJson(messageText);
+            newMessage.Print();
+
+        }
+
+    }
+}
