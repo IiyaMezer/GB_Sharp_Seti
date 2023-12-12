eacd64dc3ce37aed9279a2b551b9f4afffd4cfaf
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-12-12 18:18:58 +0700

    Удаление лишнего

--- Network/Server/Server.cs
+++ Network/Server/Server.cs
@@ -20,48 +20,6 @@ public class Server
 
     }
 
-    public void Start()
-    {
-        Console.WriteLine("Server started.");
-        Console.WriteLine("Waiting for messages...");
-        
-
-        try
-        {
-            while (true)
-            {
-                IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
-                byte[] buffer = udpServer.Receive(ref clientEndPoint);
-
-                if (buffer == null)
-                    break;
-
-                string messageText = Encoding.UTF8.GetString(buffer);
-
-                Message newMessage = Message.DeserializeMessageToJson(messageText);
-                newMessage.ReceiveConfirmation();
-
-           
-                byte[] confirmation = Encoding.UTF8.GetBytes("Message received");
-                udpServer.Send(confirmation, confirmation.Length, clientEndPoint);
-                if (newMessage.Text.ToLower().Equals("exit"))
-                {
-                    Console.WriteLine("Server is shutting down. Press any key to exit.");
-                    Console.ReadKey();
-                    break;
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        finally
-        {
-            udpServer.Close();
-        }
-    }
-
     public async Task StartAsync()
     {
         Console.WriteLine("Server started.");
