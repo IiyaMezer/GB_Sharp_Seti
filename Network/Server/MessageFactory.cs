ae494306f5a6131a2c7dcff61fc6f7b9c83a0bcd
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-12-12 18:54:33 +0700

    Фабрика сообщений

---
+++ Network/Server/MessageFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server;
+
+internal class MessageFactory : IMessageFactory
+{
+    public Message CreateMessage(string textInMsg, string nameFrom, string nameTo)
+    {
+        return new Message { Text = textInMsg, Sender = nameFrom, Reciver = nameTo, MessageTime = DateTime.Now };
+    }
+}
+
