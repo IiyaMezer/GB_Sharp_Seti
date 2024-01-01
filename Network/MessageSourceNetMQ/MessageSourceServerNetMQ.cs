ec6562c99b1e01b12a48059dfd71279a9ca91cc6
Ilya Mezer <mr.mezer@myttk.ru>
2024-01-01 13:54:04 +0700

    Imessage.... implemented

--- Network/MessageSourceNetMQ/MessageSourceServerNetMQ.cs
+++ Network/MessageSourceNetMQ/MessageSourceServerNetMQ.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using ChatCommon.Abstractions;
+using ChatCommon.Models;
+using NetMQ;
+using NetMQ.Sockets;
 
 namespace MessageSourceNetMQ
 {
-    internal class MessageSourceServerNetMQ
+    public class MessageSourceServerNetMQ : IMessageSourceServer<NetMQSocket>
     {
+        private ResponseSocket _serverSocket;
+
+        public MessageSourceServerNetMQ(string serverAddress)
+        {
+            _serverSocket = new ResponseSocket();
+            _serverSocket.Bind(serverAddress);
+        }
+
+        public NetMQSocket CopyEndpoint(IPEndPoint ep)
+        {
+            ResponseSocket socket = new();
+            socket.Connect($"tcp://{ep.Address}:{ep.Port}");
+            return socket;
+        }
+
+        public NetMQSocket CreateEndpoint()
+        {
+            return new ResponseSocket();
+        }
+
+        public NetMessage Receive(ref NetMQSocket ep)
+        {
+            var message = ep.ReceiveFrameString();
+            return new NetMessage { Text = message }; ;
+        }
+
+        public async Task SendAsync(NetMessage message, NetMQSocket ep)
+        {
+            ep.SendFrame(message.Text);
+
+            await Task.Delay(1000);
+        }
     }
 }
